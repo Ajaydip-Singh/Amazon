ca301c59e41a1a1e3175a3f6856f70f727ec0bda
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-09 16:02:16 +0300

    :sparkles: feat: create products endpoint

--- amazon-backend/server.js
+++ amazon-backend/server.js
@@ -1,9 +1,14 @@
 import express from "express";
+import data from "./data.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
 });
 
 app.listen(5000, () => {
