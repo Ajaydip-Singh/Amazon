2717dd3ad2529dd83cbdc5d9f29f204aa44a3661
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-09 14:23:19 +0300

    :sparkles: feat: show product card dynamically

--- amazon-frontend/src/App.js
+++ amazon-frontend/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import data from "./data";
+
 function App() {
   return (
     <div className="grid-container">
@@ -14,34 +17,40 @@ function App() {
       </header>
       <main>
         <div className="row center">
-          <div className="card">
-            <a href="product.html">
-              <img className="medium" src="./images/p1.jpeg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike Slim Shirts</h2>
+          {data.products.map((product) => (
+            <div key={product._id} className="card">
+              <a href={`/product/${product._id}`}>
+                <img
+                  className="medium"
+                  src={product.image}
+                  alt={product.name}
+                />
               </a>
-              <div className="rating">
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
+              <div className="card-body">
+                <a href={`/product/${product._id}`}>
+                  <h2>{product.name}</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">${product.price}</div>
               </div>
-              <div className="price">$120</div>
             </div>
-          </div>
+          ))}
         </div>
       </main>
       <footer className="row center">All rights reserved</footer>
