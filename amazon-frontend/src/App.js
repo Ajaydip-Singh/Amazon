14b2c83c3af1290abccc3a14f2f7a054808963d7
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-09 15:08:18 +0300

    :heavy_plus_sign: chore: setup react router

--- amazon-frontend/src/App.js
+++ amazon-frontend/src/App.js
@@ -1,30 +1,33 @@
 import React from "react";
+import BrowserRouter from "react-router-dom";
 import Product from "./components/Product";
 import data from "./data";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            amazon
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign in</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product._id} product={product} />
-          ))}
-        </div>
-      </main>
-      <footer className="row center">All rights reserved</footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              amazon
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign in</a>
+          </div>
+        </header>
+        <main>
+          <div className="row center">
+            {data.products.map((product) => (
+              <Product key={product._id} product={product} />
+            ))}
+          </div>
+        </main>
+        <footer className="row center">All rights reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
