6ddbecc112a102be380918c616d90adbf2931079
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-10 10:15:54 +0300

    :sparkles: feat: add quantity option to product details

--- amazon-frontend/src/screens/ProductScreen.js
+++ amazon-frontend/src/screens/ProductScreen.js
@@ -1,15 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { detailsProduct } from "../actions/productActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import Rating from "../components/Rating";
-import { PRODUCT_DETAILS_SUCCESS } from "../constants/productConstants";
 
 export default function ProductScreen(props) {
   const dispatch = useDispatch();
   const productId = props.match.params.id;
+  const [qty, setQty] = useState(1);
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
 
@@ -74,9 +74,30 @@ export default function ProductScreen(props) {
                     </div>
                   </li>
                   {product.countInStock > 0 && (
-                    <li>
-                      <button className="primary block">Add to Cart</button>
-                    </li>
+                    <>
+                      <li>
+                        <div className="row">
+                          <div>Qty</div>
+                          <div>
+                            <select
+                              value={qty}
+                              onChange={(e) => setQty(e.target.value)}
+                            >
+                              {[...Array(product.countInStock).keys()].map(
+                                (x) => (
+                                  <option key={x + 1} value={x + 1}>
+                                    {x + 1}
+                                  </option>
+                                )
+                              )}
+                            </select>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <button className="primary block">Add to Cart</button>
+                      </li>
+                    </>
                   )}
                 </ul>
               </div>
