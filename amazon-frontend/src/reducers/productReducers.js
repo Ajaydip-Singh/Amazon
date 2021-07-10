6cb00a0a2b2fb64d77b3f12f2e6bf1394284864e
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-10 09:56:44 +0300

    :bug: fix: add correct constant for details reducer

--- amazon-frontend/src/reducers/productReducers.js
+++ amazon-frontend/src/reducers/productReducers.js
@@ -1,6 +1,7 @@
 import {
   PRODUCT_DETAILS_FAIL,
   PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -29,7 +30,7 @@ export const productDetailsReducer = (
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
       return { loading: true };
-    case PRODUCT_LIST_SUCCESS:
+    case PRODUCT_DETAILS_SUCCESS:
       return { loading: false, product: action.payload };
     case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload };
