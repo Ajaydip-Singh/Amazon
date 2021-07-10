1257b10e765081ee21b8786508282e2a8cd71c3b
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-10 18:59:19 +0300

    :sparkles: feat: create products endpoint

--- amazon-backend/routers/productRouter.js
+++ amazon-backend/routers/productRouter.js
@@ -6,6 +6,14 @@ import data from "../data.js";
 const productRouter = express.Router();
 
 productRouter.get(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+  })
+);
+
+productRouter.get(
   "/seed",
   expressAsyncHandler(async (req, res) => {
     const createdProducts = await Product.insertMany(data.products);
