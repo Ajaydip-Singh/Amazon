3fc6e07a6b7e298353a7b0e324f3053f0be6fcf3
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-10 10:27:30 +0300

    :sparkles: feat: add cart screen

--- amazon-frontend/src/screens/ProductScreen.js
+++ amazon-frontend/src/screens/ProductScreen.js
@@ -17,6 +17,10 @@ export default function ProductScreen(props) {
     dispatch(detailsProduct(productId));
   }, [dispatch, productId]);
 
+  const addToCartHandler = () => {
+    props.history.push(`/cart/${productId}?qty=${qty}`);
+  };
+
   return (
     <div>
       {loading ? (
@@ -95,7 +99,12 @@ export default function ProductScreen(props) {
                         </div>
                       </li>
                       <li>
-                        <button className="primary block">Add to Cart</button>
+                        <button
+                          onClick={addToCartHandler}
+                          className="primary block"
+                        >
+                          Add to Cart
+                        </button>
                       </li>
                     </>
                   )}
