539fb8427d55346e0dc4fee8753efab19b93b128
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-10 09:22:37 +0300

    :art: chore: list products action to return function

--- amazon-frontend/src/actions/productActions.js
+++ amazon-frontend/src/actions/productActions.js
@@ -5,7 +5,7 @@ import {
   PRODUCT_LIST_SUCCESS,
 } from "../constants/productConstants";
 
-export const listProducts = async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
   dispatch({
     type: PRODUCT_LIST_REQUEST,
   });
--- amazon-frontend/src/screens/HomeScreen.js
+++ amazon-frontend/src/screens/HomeScreen.js
@@ -11,7 +11,7 @@ export default function HomeScreen() {
   const { loading, error, products } = productList;
 
   useEffect(() => {
-    listProducts(dispatch);
+    dispatch(listProducts());
   }, [dispatch]);
 
   return (
