09ef2cce52f42eac65d28a0e108865c422bc9e5e
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-10 13:25:50 +0300

    :card_file_box: chore: create user schema

---
+++ amazon-backend/models/userModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: string, required: true },
+    email: { type: string, required: true, unique: true },
+    password: { type: string, required: true },
+    isAdmin: { type: boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
