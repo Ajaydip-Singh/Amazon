4dbf9c2cec2195d1acd0d52d1dda63109ffbf546
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-10 18:56:46 +0300

    :sparkles: feat: create products seed endpoint

--- amazon-backend/server.js
+++ amazon-backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import data from "./data.js";
+import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 
 const app = express();
@@ -16,21 +16,8 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const productId = req.params.id;
-  const product = data.products.find((product) => product._id === productId);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product not Found" });
-  }
-});
-
 app.use("/api/users/", userRouter);
+app.use("/api/products/", productRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
