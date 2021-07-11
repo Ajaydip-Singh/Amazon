830fcd48f9ddeeb91a81ad3c0742b3010c699fed
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-11 10:30:11 +0300

    :construction: chore: create user reducer

---
+++ amazon-frontend/src/actions/userActions.js
@@ -0,0 +1,33 @@
+import Axios from "axios";
+import {
+  USER_SIGNIN_FAIL,
+  USER_SIGNIN_REQUEST,
+  USER_SIGNIN_SUCCESS,
+} from "../constants/userConstants";
+
+export const signIn = (email, password) => async (dispatch) => {
+  dispatch({
+    type: USER_SIGNIN_REQUEST,
+    payload: { email, password },
+  });
+
+  try {
+    const { data } = Axios.post("/api/users/signin", {
+      email,
+      password,
+    });
+    dispatch({
+      type: USER_SIGNIN_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (err) {
+    dispatch({
+      type: USER_SIGNIN_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
