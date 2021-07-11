406bc4c7e431d1399a083e84d5a85abb0b92feae
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-11 18:40:04 +0300

    :construction: chore: add register action

--- amazon-frontend/src/actions/userActions.js
+++ amazon-frontend/src/actions/userActions.js
@@ -1,10 +1,45 @@
 import Axios from "axios";
 import {
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
   USER_SIGNIN_FAIL,
   USER_SIGNIN_REQUEST,
   USER_SIGNIN_SUCCESS,
   USER_SIGNOUT,
 } from "../constants/userConstants";
+
+export const register = (name, email, password) => async (dispatch) => {
+  dispatch({
+    type: USER_REGISTER_REQUEST,
+    payload: { name, email, password },
+  });
+
+  try {
+    const { data } = await Axios.post("/api/users/register", {
+      name,
+      email,
+      password,
+    });
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+    dispatch({
+      type: USER_SIGNIN_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (err) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
 
 export const signIn = (email, password) => async (dispatch) => {
   dispatch({
