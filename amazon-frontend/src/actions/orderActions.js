236d4cc119124cc6d08d0c46479900334602ace5
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-21 15:55:07 +0300

    :construction: chore: dispatch order action

---
+++ amazon-frontend/src/actions/orderActions.js
@@ -0,0 +1,38 @@
+import {
+  ORDER_CREATE_FAIL,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+} from "../constants/orderConstants";
+import Axios from "axios";
+import { CART_EMPTY } from "../constants/cartConstants";
+
+export const createOrder = (order) => async (dispatch, getState) => {
+  dispatch({
+    type: ORDER_CREATE_REQUEST,
+    payload: order,
+  });
+
+  try {
+    const {
+      user: { userInfo },
+    } = getState();
+    const { data } = await Axios.post("/api/orders", order, {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    });
+    dispatch({
+      type: ORDER_CREATE_SUCCESS,
+      payload: data.order,
+    });
+
+  } catch (error) {
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload:
+        error.response && error.response.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
