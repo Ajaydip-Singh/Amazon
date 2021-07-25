e9b4113b75fc3192a08573d201be6bc2ead5d8db
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-25 19:16:15 +0300

    :sparkles: feat: create wrapper for admin only route

---
+++ amazon-frontend/src/components/AdminRoute.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Redirect, Route } from "react-router-dom";
+
+export default function AdminRoute({ component: Component, ...rest }) {
+  const user = useSelector((state) => state.user);
+  const { userInfo } = user;
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        userInfo && userInfo.isAdmin ? (
+          <Component {...props}></Component>
+        ) : (
+          <Redirect to="/signin" />
+        )
+      }
+    ></Route>
+  );
+}
