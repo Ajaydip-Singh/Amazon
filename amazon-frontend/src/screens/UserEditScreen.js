b91027942ca516edbf00d7d990f012e8d9b0fcaa
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-26 22:38:06 +0300

    :lipstick: style: create edit user screen

---
+++ amazon-frontend/src/screens/UserEditScreen.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { detailsUser } from "../actions/userActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+
+export default function UserEditScreen(props) {
+  const userId = props.match.params.id;
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isSeller, setIsSeller] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!user) {
+      dispatch(detailsUser(userId));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
+      setIsSeller(user.isSeller);
+      setIsAdmin(user.isAdmin);
+    }
+  }, [dispatch, userId, user]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <h1>Edit user {name}</h1>
+        </div>
+        {loading ? (
+          <LoadingBox></LoadingBox>
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <>
+            <div>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                placeholder="Enter name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                id="email"
+                value={email}
+                placeholder="Enter email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="isSeller">Is Seller</label>
+              <input
+                type="checkbox"
+                id="isSeller"
+                checked={isSeller}
+                onChange={(e) => setIsSeller(e.target.checked)}
+              />
+            </div>
+            <div>
+              <label htmlFor="isAdmin">Is Admin</label>
+              <input
+                type="checkbox"
+                id="isAdmin"
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              />
+            </div>
+            <div>
+              <button type="submit" className="primary">
+                Update
+              </button>
+            </div>
+          </>
+        )}
+      </form>
+    </div>
+  );
+}
--- amazon-frontend/src/screens/UserListScreen.js
+++ amazon-frontend/src/screens/UserListScreen.js
@@ -5,7 +5,7 @@ import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import { USER_DELETE_RESET } from "../constants/userConstants";
 
-export default function UserListScreen() {
+export default function UserListScreen(props) {
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
 
@@ -61,7 +61,11 @@ export default function UserListScreen() {
                 <td>{user.isSeller ? "YES" : "NO"}</td>
                 <td>{user.isAdmin ? "YES" : "NO"}</td>
                 <td>
-                  <button type="button" className="small">
+                  <button
+                    type="button"
+                    className="small"
+                    onClick={() => props.history.push(`/user/${user._id}/edit`)}
+                  >
                     Edit
                   </button>
                   <button
