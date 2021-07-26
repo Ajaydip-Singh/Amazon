23cea4fca007a9f18d54c0c1a5e2d5a68f2bf624
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-26 15:33:05 +0300

    :sparkles: feat: implement delete product for admin user

--- amazon-frontend/src/screens/ProductListScreen.js
+++ amazon-frontend/src/screens/ProductListScreen.js
@@ -1,9 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { createProduct, listProducts } from "../actions/productActions";
+import {
+  createProduct,
+  deleteProduct,
+  listProducts,
+} from "../actions/productActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
-import { PRODUCT_CREATE_RESET } from "../constants/productConstants";
+import {
+  PRODUCT_CREATE_RESET,
+  PRODUCT_DELETE_RESET,
+} from "../constants/productConstants";
 
 export default function ProductListScreen(props) {
   const productList = useSelector((state) => state.productList);
@@ -17,6 +24,13 @@ export default function ProductListScreen(props) {
     product: createdProduct,
   } = productCreate;
 
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete;
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -24,15 +38,19 @@ export default function ProductListScreen(props) {
       dispatch({ type: PRODUCT_CREATE_RESET });
       props.history.push(`/product/${createdProduct._id}/edit`);
     }
+    if (successDelete) {
+      dispatch({ type: PRODUCT_DELETE_RESET });
+    }
     dispatch(listProducts());
-  }, [createdProduct, dispatch, props.history, successCreate]);
+  }, [createdProduct, dispatch, props.history, successCreate, successDelete]);
 
-  const deleteHandler = () => {
-    //   Dispatch delete action
+  const deleteHandler = (product) => {
+    if (window.confirm(`Are you sure you want to delete ${product.name}`)) {
+      dispatch(deleteProduct(product._id));
+    }
   };
 
   const createHandler = () => {
-    // Dispatch created product action
     dispatch(createProduct());
   };
 
@@ -44,6 +62,8 @@ export default function ProductListScreen(props) {
           Create Product
         </button>
       </div>
+      {loadingDelete && <LoadingBox></LoadingBox>}
+      {errorDelete && <MessageBox variant="danger">{errorDelete}</MessageBox>}
       {loadingCreate && <LoadingBox></LoadingBox>}
       {errorCreate && <MessageBox variant="danger">{errorCreate}</MessageBox>}
       {loading ? (
