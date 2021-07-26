87966ed18b144722b61dc1859d2829a064c967cf
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-26 10:57:10 +0300

    :sparkles: feat: create edit product functionality

--- amazon-frontend/src/screens/ProductEditScreen.js
+++ amazon-frontend/src/screens/ProductEditScreen.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { detailsProduct } from "../actions/productActions";
+import { detailsProduct, updateProduct } from "../actions/productActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import { PRODUCT_UPDATE_RESET } from "../constants/productConstants";
 
 export default function ProductEditScreen(props) {
   const productId = props.match.params.id;
@@ -18,9 +19,21 @@ export default function ProductEditScreen(props) {
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
 
+  const productUpdate = useSelector((state) => state.productUpdate);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = productUpdate;
+
   const dispatch = useDispatch();
   useEffect(() => {
-    if (!product || productId !== product._id) {
+    if (successUpdate) {
+      props.history.push("/productlist");
+    }
+
+    if (!product || productId !== product._id || successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET });
       dispatch(detailsProduct(productId));
     } else {
       setName(product.name);
@@ -31,11 +44,22 @@ export default function ProductEditScreen(props) {
       setBrand(product.brand);
       setDescription(product.description);
     }
-  }, [dispatch, productId, product]);
+  }, [props, successUpdate, dispatch, productId, product]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    // Dispatch update product
+    dispatch(
+      updateProduct({
+        _id: product._id,
+        name,
+        price,
+        image,
+        category,
+        countInStock,
+        brand,
+        description,
+      })
+    );
   };
 
   return (
@@ -44,6 +68,8 @@ export default function ProductEditScreen(props) {
         <div>
           <h1>Edit Product</h1>
         </div>
+        {loadingUpdate && <LoadingBox></LoadingBox>}
+        {errorUpdate && <MessageBox variant="danger">{errorUpdate}</MessageBox>}
         {loading ? (
           <LoadingBox></LoadingBox>
         ) : error ? (
