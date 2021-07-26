ab255c0084224e816455fa45145eea0bf48a27e8
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-26 17:50:14 +0300

    :sparkles: feat: create deliver order endpoint for admin

--- amazon-backend/routers/orderRouter.js
+++ amazon-backend/routers/orderRouter.js
@@ -85,6 +85,23 @@ orderRouter.put(
   })
 );
 
+orderRouter.put(
+  "/:id/deliver",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+      const updatedOrder = await order.save();
+      res.send({ message: "Order delivered", order: updatedOrder });
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
+
 orderRouter.delete(
   "/:id",
   isAuth,
