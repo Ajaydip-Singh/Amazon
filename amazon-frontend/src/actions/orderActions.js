f42fe7ebba1748da1f374fa1ff5c23cd8ef7896c
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-26 15:48:43 +0300

    :sparkles: feat: create orders list screen for admin

--- amazon-frontend/src/actions/orderActions.js
+++ amazon-frontend/src/actions/orderActions.js
@@ -5,6 +5,9 @@ import {
   ORDER_DETAILS_FAIL,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
+  ORDER_LIST_FAIL,
+  ORDER_LIST_REQUEST,
+  ORDER_LIST_SUCCESS,
   ORDER_MINE_LIST_FAIL,
   ORDER_MINE_LIST_REQUEST,
   ORDER_MINE_LIST_SUCCESS,
@@ -137,6 +140,36 @@ export const listOrderMine = () => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: ORDER_MINE_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listOrders = () => async (dispatch, getState) => {
+  dispatch({
+    type: ORDER_LIST_REQUEST,
+  });
+
+  try {
+    const {
+      user: { userInfo },
+    } = getState();
+
+    const { data } = await Axios.get("/api/orders", {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    });
+    dispatch({
+      type: ORDER_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
