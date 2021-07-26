712a51ab744c8db5fb6ffb5d5f0212a67ecbbab8
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-26 22:10:05 +0300

    :sparkles: feat: delete user endpoint for admins

--- amazon-backend/routers/userRouter.js
+++ amazon-backend/routers/userRouter.js
@@ -101,4 +101,19 @@ userRouter.get(
   })
 );
 
+userRouter.delete(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      const deletedUser = await user.remove();
+      res.send({ message: "User deleted", user: deletedUser });
+    } else {
+      res.status(404).send({ message: "User Not Found" });
+    }
+  })
+);
+
 export default userRouter;
