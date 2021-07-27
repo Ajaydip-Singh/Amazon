8ef5b3ab2c75c50977f4db2ca48a136db63f6f89
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-27 15:41:27 +0300

    :sparkles: feat: add spinning loading icon

--- amazon-frontend/src/components/LoadingBox.js
+++ amazon-frontend/src/components/LoadingBox.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function LoadingBox() {
   return (
     <div>
-      <i className="fas fa-spinner">Loading...</i>
+      <i className="fas fa-spinner fa-spin"></i> Loading...
     </div>
   );
 }
